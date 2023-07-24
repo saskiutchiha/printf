8bf877d6eeda431cec1b573c24248df8aa8bfe30
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-23 18:39:48 -0700

    Update hhhhhhh.c

--- hhhhhhh.c
+++ hhhhhhh.c
@@ -1,11 +1,38 @@
 #include <string.h>
 #include "main.h"
+#include <stdarg.h>
 int _printf(const char *format, ...)
 {
-int i,a= strlen(format);
+char *x,z;
+int i,j,a= strlen(format),b,c=0;
+va_list args;
+va_start(args,format);
 for (i=0;i<a;i++)
 {
-_putchar(*(format+i));
+if(*(format+i) == '%' && *(format+i+1) == 'c')
+{
+z = va_arg(args,int);
+_putchar(z);
+i++;
+c++;
 }
-return (a);
+else if (*(format+i) == '%' && *(format+i+1) == 's')
+{
+  x = va_arg(args,char*);
+  b = strlen(x);
+for (j=0; j <b;j++)
+  {
+  _putchar(*(x+j));
+    c++;
+  }
+  i++;
+}
+else
+{
+_putchar(*(format+i));
+c++;
+}
+}
+va_end(args);
+return (c);
 }
