38cdd973e53dc2f9e1e19c8ce04a56b1b850180b
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-24 17:17:42 -0700

    Update hhhhhhh.c

--- hhhhhhh.c
+++ hhhhhhh.c
@@ -66,7 +66,7 @@ else if (*(format+i) == '%' && (*(format+i+1) == 'd' || *(format+i+1) == 'i'))
 }
   else if (*(format+i) == '%' && *(format+i+1) == 'b' )
   {
-    y = av_arg(args,int);
+    y = va_arg(args,int);
     n=1;
     for (j=0; y!= 0;j++)
       {
